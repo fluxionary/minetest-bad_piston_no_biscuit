b5e4bc5714ce5b3e003f3cecc685d37a2032fcad
y <y@y>
2019-01-15 00:17:00 -0800

    initial

---
+++ init.lua
@@ -0,0 +1,24 @@
+minetest.log('[biscuit] bad piston no biscuit')
+
+mesecon.register_mvps_stopper('terumet:mach_asmelt')
+mesecon.register_mvps_stopper('terumet:mach_asmelt_lit')
+mesecon.register_mvps_stopper('terumet:mach_htfurn')
+mesecon.register_mvps_stopper('terumet:mach_htfurn_lit')
+mesecon.register_mvps_stopper('terumet:mach_vulcan')
+
+mesecon.register_mvps_stopper('terumet:mach_hray')
+mesecon.register_mvps_stopper('terumet:mach_thermdist')
+mesecon.register_mvps_stopper('terumet:mach_thermobox')
+
+mesecon.register_mvps_stopper('terumet:mach_htr_entropy')
+mesecon.register_mvps_stopper('terumet:mach_htr_furnace')
+mesecon.register_mvps_stopper('terumet:mach_htr_furnace_lit')
+mesecon.register_mvps_stopper('terumet:mach_htr_solar')
+
+mesecon.register_mvps_stopper('terumet:mach_repm')
+mesecon.register_mvps_stopper('terumet:mach_hl_input')
+mesecon.register_mvps_stopper('terumet:mach_lavam')
+mesecon.register_mvps_stopper('terumet:mach_lavam_lit')
+mesecon.register_mvps_stopper('terumet:mach_meseg')
+mesecon.register_mvps_stopper('terumet:mach_crusher')
+mesecon.register_mvps_stopper('terumet:mach_crusher_lit')
